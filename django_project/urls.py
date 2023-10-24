ece2e55f38595034bdcbdf4add6d70ad47df58a8
utquattro <utquattro@gmail.com>
2023-10-24 18:01:39 +0300

    title="е_шоп API",

--- django_project/urls.py
+++ django_project/urls.py
@@ -27,7 +27,7 @@ from drf_yasg import openapi  # new
 
 schema_view = get_schema_view(  # new
     openapi.Info(
-        title="Snippets API",
+        title="е_шоп API",
         default_version='v1',
         description="Test description",
         terms_of_service="https://www.google.com/policies/terms/",
