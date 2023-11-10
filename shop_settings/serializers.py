20b6b4aa185b79cd31fc4f1296bcf68205825300
utquattro <utquattro@gmail.com>
2023-11-10 10:11:34 +0300

    change serializer settings

--- shop_settings/serializers.py
+++ shop_settings/serializers.py
@@ -5,13 +5,13 @@ from .models import PaySystem, Socical, MarketingBanner, Settings
 class SocicalSerializer(serializers.ModelSerializer):
     class Meta:
         model = Socical
-        fields = ('name', 'description', 'img_url', 'active')
+        fields = ('name', 'description','active')
 
 
 class PaySystemSerializer(serializers.ModelSerializer):
     class Meta:
         model = PaySystem
-        fields = ('name', 'description', 'img_url', 'active')
+        fields = ('name', 'description', 'active')
 
 
 class MarketingBannerSerializer(serializers.ModelSerializer):
