11d3ec75ae85b98631cbd139f4b57e05d3c206dd
utquattro <utquattro@gmail.com>
2023-12-13 16:48:36 +0300

    admin.site.register(Session)

--- cart/urls.py
+++ cart/urls.py
@@ -4,8 +4,8 @@ from cart.views import  cart_add, cart_detail, cart_remove
 urlpatterns = [
 
 
-    path('cart', cart_detail),
-    path('cart/add', cart_add),
-    path('cart/remove', cart_remove),
+    path('cart/', cart_detail),
+    path('cart/add/', cart_add),
+    path('cart/remove/', cart_remove),
 ]
 
