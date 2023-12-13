609bd138f537e5869c3be9f21da9a4da1f305a1f
utquattro <utquattro@gmail.com>
2023-12-13 15:36:24 +0300

    add CSRF_TRUSTED_ORIGINS = ['https://*']

--- django_project/settings.py
+++ django_project/settings.py
@@ -140,8 +140,9 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 CORS_ORIGIN_ALLOW_ALL = True
 SECURE_CROSS_ORIGIN_OPENER_POLICY = None
 
-
+CSRF_TRUSTED_ORIGINS = ['https://*']
 CSRF_USE_SESSIONS = True
 SESSION_COOKIE_SECURE = True
 SESSION_ENGINE = 'django.contrib.sessions.backends.db'
 CART_SESSION_ID = 'cart'
+
