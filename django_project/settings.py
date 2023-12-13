d838c6083a83c879d7c0a3cdf7fffef35eb9980c
utquattro <utquattro@gmail.com>
2023-12-13 15:38:29 +0300

    change CSRF_TRUSTED_ORIGINS = localhost

--- django_project/settings.py
+++ django_project/settings.py
@@ -140,7 +140,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 CORS_ORIGIN_ALLOW_ALL = True
 SECURE_CROSS_ORIGIN_OPENER_POLICY = None
 
-CSRF_TRUSTED_ORIGINS = ['https://*']
+CSRF_TRUSTED_ORIGINS = ['localhost']
 CSRF_USE_SESSIONS = True
 SESSION_COOKIE_SECURE = True
 SESSION_ENGINE = 'django.contrib.sessions.backends.db'
