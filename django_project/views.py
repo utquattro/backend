1257d465b3bfb4e5b44b448434c566153a29a017
utquattro <utquattro@gmail.com>
2024-01-08 23:43:26 +0300

    change response login\singup

--- django_project/views.py
+++ django_project/views.py
@@ -19,7 +19,7 @@ def signup(request):
         user.set_password(request.data['password'])
         user.save()
         token = Token.objects.create(user=user)
-        return Response({'token': token.key, 'user': serializer.data})
+        return Response({'token': token.key})
     return Response(serializer.errors, status=status.HTTP_200_OK)
 
 @api_view(['POST'])
@@ -30,7 +30,7 @@ def login(request):
             return Response({"error": "ivalid code"}, status=status.HTTP_404_NOT_FOUND)
         token, created = Token.objects.get_or_create(user=user)
         serializer = UserSerializer(user)
-        return Response({'token': token.key, 'user': serializer.data})
+        return Response({'token': token.key})
     except BaseException as e:
         return Response({str(e)})
 
