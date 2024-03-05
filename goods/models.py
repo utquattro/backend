244e02d8158c033224edd27187be71befa32a296
utquattro <utquattro@gmail.com>
2024-03-05 23:01:06 +0300

    add raiting

--- goods/models.py
+++ goods/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 from django_project.base_model import BaseModel, ImgModel
 from django.urls import reverse
-from django.core.validators import MinValueValidator
+from django.core.validators import MinValueValidator, MaxValueValidator
 
 
 class Brand(BaseModel):
@@ -88,6 +88,8 @@ class ProductSku(BaseModel):
     sku = models.TextField(max_length=100, blank=False, null=True, unique=True,
                            verbose_name='Артикул товара')
     characteristics = models.ManyToManyField(Characteristic, related_name="sku_char")
+    raiting = models.DecimalField(blank=True, null=True, max_digits=2, decimal_places=1,
+                                  validators=[MinValueValidator(0), MaxValueValidator(10)])
     price = models.PositiveIntegerField(blank=False, null=True, validators=[MinValueValidator(1)],
                                         verbose_name='Цена')
     stock = models.PositiveIntegerField(blank=False, null=True, validators=[MinValueValidator(0)],
