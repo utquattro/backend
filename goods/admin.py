b6670ee6489f656cfec6a7535dfc2cc7a1dc814a
utquattro <utquattro@gmail.com>
2024-04-15 12:51:41 +0300

    get_title_length

--- goods/admin.py
+++ goods/admin.py
@@ -59,13 +59,15 @@ class ProductAdmin(BaseAdmin):
 
 class ProductItemAdmin(BaseAdmin):
     list_display = (
-        'id', 'category', 'name', 'title',  'get_title_length', 'rate', 'brand', 'sku', 'price', 'stock', 'description',
+        'id', 'category', 'name', 'title', 'get_title_length', 'rate', 'brand', 'sku', 'price', 'stock', 'description',
         'img_url',
         'created_at', 'updated_at', 'active')
-    list_filter = ('title', )
+    list_filter = ('title',)
 
     def get_title_length(self, obj):
         return len(obj.title)
+
+    get_title_length.admin_order_field = '-title'
 
 
 admin.site.register(Categorie, CategorieAdmin)
